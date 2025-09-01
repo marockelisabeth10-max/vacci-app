5f115bcc9649c236996ac17ca2a90fdb7c53d89b
EliseMarock <elisemarock@gmail.com>
2025-09-01 15:44:20 +0100

    setup taiwindcss

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="bg-blue-900 text-white text-2xl p-4 text-center font-bold">
+      Login Page
+    </div>
+  );
+};
+
+export default Login;
